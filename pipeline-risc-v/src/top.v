770171deef104aa86c207545f5cae83bf3ad69ee
TomasLAbreu <tomasld.abreu@gmail.com>
2022-05-24 21:18:41 +0100

    Add pipeline project

---
+++ pipeline-risc-v/src/top.v
@@ -0,0 +1,48 @@
+module top (
+	clk,
+	reset,
+	WriteData,
+	DataAdr,
+	MemWrite,
+	func3_debug
+);
+	input wire clk;
+	input wire reset;
+	
+	output wire [31:0] WriteData;
+	output wire [31:0] DataAdr;
+	output wire MemWrite;
+	
+	wire [31:0] PC;
+	wire [31:0] Instr;
+	wire [31:0] ReadData;
+	
+	output wire [1:0] func3_debug;
+
+    assign func3_debug = Instr[13:12];
+
+	riscvpipeline rvpipeline(
+		clk,
+		reset,
+		PC,
+		Instr,
+		MemWrite,
+		DataAdr,
+		WriteData,
+		ReadData
+	);
+	
+	imem imem(
+		.a(PC),
+		.rd(Instr)
+	);
+	
+	dmem dmem(
+		.clk(clk),
+		.we(MemWrite),
+		.a(DataAdr),
+		.wd(WriteData),
+		.be(Instr[13:12]),
+		.rd(ReadData)
+	);
+endmodule
