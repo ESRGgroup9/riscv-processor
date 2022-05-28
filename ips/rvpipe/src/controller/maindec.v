4bbce268799287b91a583b507af5d43abdf30d77
DuarteRodrigues-88259-UM <duartemiguelinho@gmail.com>
2022-05-28 12:18:29 +0100

    update tcl and add riscv pipe ip

---
+++ ips/rvpipe/src/controller/maindec.v
@@ -0,0 +1,71 @@
+module maindec (
+	op,
+	funct3,
+	
+	ResultSrc,
+	MemWrite,
+	ALUSrc,
+	RegWrite,
+	PCResultSrc,
+	ImmSrc,
+	ALUOp
+);
+	input wire [6:0] op;
+	input wire [2:0] funct3;
+
+	output wire [2:0] ResultSrc;
+	output wire MemWrite;
+	output wire ALUSrc;
+	output wire RegWrite;
+	// selects PC between ALUResult and (PC+ImmExt)
+	output wire PCResultSrc;
+	output wire [2:0] ImmSrc;
+	// ALU operation
+	output wire [1:0] ALUOp;
+
+`define CTRL_SIZE 12
+	reg [`CTRL_SIZE-1:0] controls;
+
+     reg a;
+	// 		1bit	  3bits   1bit	  1bit      3bits	   2bits  1bit 
+	assign {RegWrite, ImmSrc, ALUSrc, MemWrite, ResultSrc, ALUOp, PCResultSrc} = controls;
+	
+	always @(*) begin
+		case (op)
+		    // 3: load - lx, rd, imm(rs1)
+			7'b000_0011: controls = 12'b_1_000_1_0_001_00_0;
+			
+			// 0x23 = 35: store - sx, rs2, imm(rs1) 
+			7'b010_0011: controls = 12'b_0_001_1_1_111_00_0;
+
+			// 0x33 = 51: Type R - xxx, rd, rs1, rs2
+			//7'b011_0011: controls = 12'b_1_xxx_0_0_000_10_0;
+			7'b011_0011: controls = 12'b_1_000_0_0_000_10_0;
+
+			// 0x63 = 99: branch - bxx, rs1, rs2, label
+			7'b110_0011: controls = 12'b_0_010_0_0_000_01_0;
+			
+			// 0x13 = 19: Type I - xxxi, rd, rs1, imm
+			7'b001_0011: controls = 12'b_1_000_1_0_000_10_0;
+			
+			// 0x6F = 111: jump and link - jal, rd, label
+			7'b110_1111: controls = 12'b_1_011_0_0_010_00_0;
+			
+			// 23: auipc rd, upimm (U)
+			//7'b001_0111: controls = 12'b_1_100_x_0_100_xx_0;
+			7'b001_0111: controls = 12'b_1_100_0_0_101_00_0;
+
+			// 55: lui rd, upimm (U)
+			// 7'b011_0111: controls = 12'b_1_100_x_0_011_xx_0;
+			//7'b011_0111: controls = 12'b_1_100_x_0_110_xx_0;
+			7'b011_0111: controls = 12'b_1_100_0_0_011_00_0;
+
+			// 103: jalr rd, rs1, imm (I)
+			7'b110_0111: controls = 12'b_1_000_1_0_010_10_1;
+
+			// default: controls = {12{1'bx}};
+			default: controls = {12{1'b0}};
+		endcase
+		a = 0;
+end
+endmodule
---
+++ ips/rvpipe/src/datapath/pipelineDE_dp.v
@@ -0,0 +1,94 @@
+module pipelineDE_dp(
+	input clk,
+	input reset,
+	input FlushE,
+
+	// inputs
+	input wire [2:0] InstrD,
+	input wire [31:0] RD1D,
+	input wire [31:0] RD2D,
+	input wire [31:0] PCD,
+	input wire [4:0] Rs1D,
+	input wire [4:0] Rs2D,
+	input wire [4:0] RdD,
+	input wire [31:0] ImmExtD,
+	input wire [31:0] PCPlus4D,
+
+	// outputs
+	output wire [2:0] InstrE,
+	output wire [31:0] RD1E,
+	output wire [31:0] RD2E,
+	output wire [31:0] PCE,
+	output wire [4:0] Rs1E,
+	output wire [4:0] Rs2E,
+	output wire [4:0] RdE,
+	output wire [31:0] ImmExtE,
+	output wire [31:0] PCPlus4E
+);
+
+wire clear = reset | FlushE;
+
+flopr #(3) instrreg(
+	clk,
+	reset,
+	InstrD,
+	InstrE
+);
+
+flopr #(32) rd1reg(
+	clk,
+	clear,
+	RD1D,
+	RD1E
+);
+
+flopr #(32) rd2reg(
+	clk,
+	clear,
+	RD2D,
+	RD2E
+);
+
+flopr #(32) pcreg(
+	clk,
+	clear,
+	PCD,
+	PCE
+);
+
+flopr #(5) rs1reg(
+	clk,
+	clear,
+	Rs1D,
+	Rs1E
+);
+
+flopr #(5) rs2reg(
+	clk,
+	clear,
+	Rs2D,
+	Rs2E
+);
+
+flopr #(5) rdreg(
+	clk,
+	clear,
+	RdD,
+	RdE
+);
+
+flopr #(32) immextreg(
+	clk,
+	clear,
+	ImmExtD,
+	ImmExtE
+);
+
+flopr #(32) pcplusreg(
+	clk,
+	clear,
+	PCPlus4D,
+	PCPlus4E
+);
+
+endmodule
